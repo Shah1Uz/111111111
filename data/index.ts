4470f14752e867f87283c465e6251b42bc09e4e5
Shah1Uz <shahuzmusic@gmail.com>
2024-12-18 12:24:49 +0500

    first commit

---
+++ data/index.ts
@@ -0,0 +1,217 @@
+export const navItems = [
+  { name: "Men xaqimda", link: "#about" },
+  { name: "Loyihalar", link: "#projects" },
+  { name: "Mijozlar", link: "#testimonials" },
+  { name: "Bog'lanish", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Veb saytlar ishlab chiqish , Telegram botlar yaratish !",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Innovatsion xizmatlar Dunyo bo'ylab",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Men doimo yaxshilashga harakat qilaman",
+    description: "Mening texnik to'plamim",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Ijtimoiy tarmoqlarni yuritish (SMM) , Qidiruv tizimlarida optimallashtirish (SEO)",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Websitelarni sifatli darajada qurish",
+    description: "WebSite xizmati",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Loyihani men bilan birgalikda boshlamoqchimisiz?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "VK Music bot",
+    des: "Telegram platformasi orqali siz istalgan musiqa videolar yuklashingiz mumkin.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Yoom - Video konferentsiya uchun ilova",
+    des: "Yoom bilan video konferentsiya tajribangizni soddalashtiring. Uzluksiz",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "AI Image SaaS - Canva ilovasi",
+    des: "AI xususiyatlari va to'lovlar va kreditlarga ega HAQIQIY dasturiy ta'minot xizmati sifatida ilova.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Animatsiyalangan Apple Iphone 3D veb-sayti",
+    des: "GSAP animatsiyalarini birlashtirgan Apple iPhone 15 Pro veb-saytini qayta yaratd...",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Shahzod bilan hamkorlik qilish mutlaq zavq edi. Uning professionalligi, tezkorligi va g'ayrioddiy natijalarga erishishga sadoqati butun loyihamiz davomida yaqqol namoyon bo'ldi. Adrianning rivojlanishning har bir jabhasiga bo'lgan ishtiyoqi haqiqatan ham ajralib turadi. Agar siz veb-saytingizni yuksaltirishga va brendingizni yuksaltirishga intilayotgan bo'lsangiz, Shahzod ideal hamkordir.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Shahzod bilan hamkorlik qilish mutlaq zavq edi. Uning professionalligi, tezkorligi va g'ayrioddiy natijalarga erishishga sadoqati butun loyihamiz davomida yaqqol namoyon bo'ldi. Adrianning rivojlanishning har bir jabhasiga bo'lgan ishtiyoqi haqiqatan ham ajralib turadi. Agar siz veb-saytingizni yuksaltirishga va brendingizni yuksaltirishga intilayotgan bo'lsangiz, Shahzod ideal hamkordir.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Shahzod bilan hamkorlik qilish mutlaq zavq edi. Uning professionalligi, tezkorligi va g'ayrioddiy natijalarga erishishga sadoqati butun loyihamiz davomida yaqqol namoyon bo'ldi. Adrianning rivojlanishning har bir jabhasiga bo'lgan ishtiyoqi haqiqatan ham ajralib turadi. Agar siz veb-saytingizni yuksaltirishga va brendingizni yuksaltirishga intilayotgan bo'lsangiz, Shahzod ideal hamkordir.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Shahzod bilan hamkorlik qilish mutlaq zavq edi. Uning professionalligi, tezkorligi va g'ayrioddiy natijalarga erishishga sadoqati butun loyihamiz davomida yaqqol namoyon bo'ldi. Adrianning rivojlanishning har bir jabhasiga bo'lgan ishtiyoqi haqiqatan ham ajralib turadi. Agar siz veb-saytingizni yuksaltirishga va brendingizni yuksaltirishga intilayotgan bo'lsangiz, Shahzod ideal hamkordir.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Shahzod bilan hamkorlik qilish mutlaq zavq edi. Uning professionalligi, tezkorligi va g'ayrioddiy natijalarga erishishga sadoqati butun loyihamiz davomida yaqqol namoyon bo'ldi. Adrianning rivojlanishning har bir jabhasiga bo'lgan ishtiyoqi haqiqatan ham ajralib turadi. Agar siz veb-saytingizni yuksaltirishga va brendingizni yuksaltirishga intilayotgan bo'lsangiz, Shahzod ideal hamkordir.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend muhandisi stajyor",
+    desc: "Interaktivlikni oshirib, React.js yordamida veb-platformani ishlab chiqishda yordam beraman.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobil ilova ishlab chiqaruvchisi - JSM",
+    desc: "React Native yordamida iOS va Android platformalari uchun mobil ilova ishlab chiqilgan .",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Developer loyihasi",
+    desc: "Dastlabki kontseptsiyadan tortib ilovalar do'konlarida joylashtirishgacha bo'lgan mijoz uchun mobil ilovani ishlab chiqishga rahbarlik qildim.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Yetakchi Frontend Developer",
+    desc: "Zamonaviy frontend texnologiyalaridan foydalangan holda foydalanuvchiga qaratilgan xususiyatlar ishlab chiqilgan va qo'llab-quvvatlangan.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
